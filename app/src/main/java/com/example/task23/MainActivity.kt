8929d2b6037dcd9a3279ecec6bd4ed8211c0e435
user <frostkslo1@yandex.ru>
2022-01-12 12:57:17 +0300

    Сделано окошко логина

--- app/src/main/java/com/example/task23/MainActivity.kt
+++ app/src/main/java/com/example/task23/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.task23
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val thread = MyThread(this)
+        thread.start()
+    }
+}
+
+// Делаю вид что, что-то грузится
+class MyThread(val context: MainActivity): Thread(){
+
+    override fun run() {
+        sleep(3000)
+        context.startActivity(Intent(context, LoginActivity::class.java))
+        context.finish()
+        super.run()
     }
 }
