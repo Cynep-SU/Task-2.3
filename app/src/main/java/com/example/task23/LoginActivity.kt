0a2768388806665c4c21cc466c06327296248ed1
user <frostkslo1@yandex.ru>
2022-01-14 10:25:22 +0300

    activity_reg.xml is done

--- app/src/main/java/com/example/task23/LoginActivity.kt
+++ app/src/main/java/com/example/task23/LoginActivity.kt
@@ -1,11 +1,16 @@
 package com.example.task23
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+    fun RegOnclick(v: View){
+        startActivity(Intent(this, Reg::class.java))
     }
 }
